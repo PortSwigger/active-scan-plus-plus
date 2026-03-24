1e20e9ff39be4def4a5166117c854f46780363d5
Hannah <58562826+Hannah-PortSwigger@users.noreply.github.com>
2026-03-24 15:11:01 +0000

    Bump version number

--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -13,7 +13,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class BurpExtender implements IBurpExtender, IExtensionStateListener, BurpExtension {
     private static final String name = "ActiveScan++";
-    private static final String version = "2.0.9";
+    private static final String version = "2.0.10";
     public boolean unloaded = false;
     static ConcurrentHashMap<String, Boolean> hostsToSkip = new ConcurrentHashMap<>();
     static final AtomicBoolean verifyGeminiAccess = new AtomicBoolean(false);
